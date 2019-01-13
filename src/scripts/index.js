29c91bcd58d75871dd6957a0f2496224597547e4
Stephen Fairbanks <ghosthorses@outlook.com>
2019-01-13 17:14:45 +0000

    Feat: JS setup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,5 @@
-import "../styles/index.scss";
+import '../styles/index.scss';
 
-const test = [1, 2, 3];
-
-const filtered = [...test].filter(item => item <= 2);
-
-console.log(filtered);
+(() => {
+  console.log('Hello mum.');
+})();
